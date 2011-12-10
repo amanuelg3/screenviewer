362b535c98406c963748d832b5b78c57d5dd1419
j.salkevicius <j.salkevicius@localhost>
2011-12-10 18:19:47 +0200

    Mouse packet added

--- src/scrview/mousepacket.cpp
+++ src/scrview/mousepacket.cpp
@@ -2,4 +2,45 @@
 
 MousePacket::MousePacket()
 {
+    md = NULL;
+    type = 1;
+}
+
+void MousePacket::makePacket()
+{
+    if (md == NULL) {
+        currentPacket = NULL;
+        return;
+    }
+    currentPacket = new QByteArray();
+    QDataStream out(currentPacket, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_4_2);
+    blockSize = md->getScreen()->size();
+    out << (quint16) blockSize;
+    out << (quint16) type;
+    out << md->x;
+    out << md->y;
+    out << md->isLeftKey;
+    out << md->isRightKey;
+}
+
+void MousePacket::setNewContent(MouseData *md)
+{
+    delete this->md;
+    this->md = md;
+}
+
+static MouseData* MousePacket::analyzePacket(QDataStream& in)
+{
+    MouseData* tmp;
+    in >> tmp->x;
+    in >> tmp->y;
+    in >> tmp->isLeftKey;
+    in >> tmp->isRightKey;
+    return tmp;
+}
+
+MouseData* SsPacket::getContent()
+{
+    return md;
 }
---
+++ src/scrview/testmousepacket.h
@@ -0,0 +1,10 @@
+#ifndef TESTMOUSEPACKET_H
+#define TESTMOUSEPACKET_H
+
+class TestMousePacket
+{
+public:
+    TestMousePacket();
+};
+
+#endif // TESTMOUSEPACKET_H

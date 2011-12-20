72450593367115a7a0c9385134b4b2eea17d6334
JuliusR <JuliusR@localhost>
2011-12-20 23:52:46 +0200

    Fixed some bugs, screenviewer is now working, need to check on different machines to see if mouse control works or not.

--- src/scrview/screenview.cpp
+++ src/scrview/screenview.cpp
@@ -11,13 +11,10 @@ ScreenView::ScreenView(QWidget *parent) :
     mouse = new Mouse();
     capturer = new Capturer(60, "JPG", 800, 600);
     z = new QTimer(this);
-    //connect(z, SIGNAL(timeout()), this, SLOT(doTest()));
-    connect(z, SIGNAL(timeout()), this, SLOT(doMouseTest()));
+    connect(z, SIGNAL(timeout()), this, SLOT(doTest()));
+    connect(z, SIGNAL(timeout()), this, SLOT(mouseSync()));
     z->start(500);
-    IPdatabase *ip = new IPdatabase;
-
-    ip->setIP("blA", "aaa");
-    ip->getIP("blA", "aaa");
+    ui->gbox_login->hide();
 }
 
 ScreenView::~ScreenView()
@@ -54,19 +51,16 @@ void ScreenView::doTest()
     }
 }
 
-void ScreenView::doMouseTest()
+void ScreenView::mouseSync()
 {
     if (!clicked)
         return;
 
     if (server)
-    {
-
-    }
+        mouse->setMouseState(mouse->getPos().x(),mouse->getPos().y(),mouse->isLeftKeyP(),mouse->isRightKeyP());
     else
-    {
         b->send(mouse->isRightKeyP(), mouse->isLeftKeyP(), mouse->getPos().x(), mouse->getPos().y());
-    }
+
 }
 
 
@@ -84,6 +78,9 @@ void ScreenView::on_serverButton_clicked()
 
 bool ScreenView::eventFilter(QObject *obj, QEvent *event)
 {
+    if (server)
+        return false;
+
     if(event->type() == QEvent::MouseButtonPress)
     {
         QMouseEvent *k = (QMouseEvent *)event;
@@ -119,5 +116,18 @@ bool ScreenView::eventFilter(QObject *obj, QEvent *event)
 
 void ScreenView::on_clientButton_clicked()
 {
+    ui->gbox_login->show();
+}
 
+void ScreenView::on_pushButton_clicked()
+{
+    /*IPdatabase *ip = new IPdatabase;
+    ip->getIP(ui->input_user->text(), ui->input_pass->text());*/
+
+    server = false;
+    b = new Client(this);
+        b->setHost("127.0.0.1");
+    //b->setHost("88.222.10.7"); test ip
+    b->connectToHost();
+    clicked = true;
 }
